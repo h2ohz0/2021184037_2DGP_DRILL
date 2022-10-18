1e2949e2e3e7e9a759333e1520880d71fca2e94e
h2ohz0 <hjy04236@naver.com>
2022-10-18 19:40:50 +0900

    예제 소스 및 자료

--- Project/character_runs_esc.py
+++ Project/character_runs_esc.py
@@ -1,7 +1,14 @@
 from pico2d import *
+
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
+
+
+running = True
+x = 0
+frame = 0
+
 
 def handle_events():
     global running
@@ -11,19 +18,19 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+    pass
 
-running = True
-x = 0
-frame=0
-while(x<800 and running):
+
+while x < 800 and running:
     clear_canvas()
-    grass.draw(400,30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
     update_canvas()
+
+    handle_events()
     frame = (frame + 1) % 8
     x += 5
-    delay(0.05)
-    get_events()
+    delay(0.01)
 
 close_canvas()
 
